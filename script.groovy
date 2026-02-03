fbeb9ef067dc574d52349fd2e36f402a5736a2c2
erarfat <er.arfat7@gmail.com>
2026-02-03 12:59:05 +0530

    Added Groovy-Vault-Hashi

--- script.groovy
+++ script.groovy
@@ -9,8 +9,8 @@ def Test(){
 }
 def Push(String user, String pass){
     echo "Pushing To Repo"
-    sh 'docker build -t 43.205.240.167:8083/java-app-v4 .'
+    sh 'docker build -t 43.205.240.167:8083/java-app-v5 .'
     sh 'docker login 43.205.240.167:8083 -u $user -p $pass'
-    sh 'docker push 43.205.240.167:8083/java-app-v4'
+    sh 'docker push 43.205.240.167:8083/java-app-v5'
 }
 return this 
